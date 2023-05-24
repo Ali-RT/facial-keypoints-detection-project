44223dcdf63ef2fa853fd7d5f0f814e15d46ede0
Ali-RT <ali.rahim.taleqani@gmail.com>
2023-05-24 13:54:10 +0000

    update cli

--- facial_keypoints_detection/cli.py
+++ facial_keypoints_detection/cli.py
@@ -2,15 +2,40 @@
 import sys
 import click
 
+@click.group()
+def cli():
+    pass
 
-@click.command()
-def main(args=None):
-    """Console script for facial_keypoints_detection."""
-    click.echo("Replace this message by putting your code into "
-               "facial_keypoints_detection.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+@cli.command()
+@click.option('--data', help='Path to the training data.')
+def train(data):
+    """
+    Train a new model.
+    """
+    # Call your function to train a model
+    # train_model(data)
+    click.echo(f"Training model with data from {data}")
 
+@cli.command()
+@click.option('--image', help='Path to the image.')
+def detect(image):
+    """
+    Detect facial landmarks in an image.
+    """
+    # Call your function to detect facial landmarks
+    # detect_facial_landmark(image)
+    click.echo(f"Detecting facial landmarks in image at {image}")
+
+@cli.command()
+@click.option('--image', help='Path to the image.')
+@click.option('--filter', help='Filter to be applied.')
+def filter(image, filter):
+    """
+    Apply a filter to a face in an image.
+    """
+    # Call your function to apply a facial filter
+    # apply_facial_filter(image, filter)
+    click.echo(f"Applying {filter} to image at {image}")
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    cli()
